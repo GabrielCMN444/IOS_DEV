311895c7da520e9bb5e5406028608ebb5af82c50
Gabriel <gab.campos08.444@gmail.com>
2023-11-16 19:34:16 -0300

    react

---
+++ Curso Ios/AulasREACT/AULA04.REACT/Aula04-REACT/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Component } from "react";
+import Exemplo from "./Components/Exemplo.jsx";
+import Header from "./Components/Header.jsx";
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header nome="Morty" />
+        <Exemplo />
+      </div>
+    );
+  }
+}
+
+export default App;
